f35adfb911970cc5e8fef674fa410a2f6622c4d9
Silas Sena <138824542+br-silassena@users.noreply.github.com>
2024-02-04 16:18:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 'use strict';
 
-const express = require('express');
+const http = require('http');
 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-
-//Router
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Crie um servidor HTTP
+const server = http.createServer((req, res) => {
+  // Lógica de resposta aqui
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Servidor Node.js está em execução!\n');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+// Defina a porta em que o servidor irá escutar
+const porta = 3000;
+
+// Faça o servidor escutar a porta especificada
+server.listen(porta, () => {
+  console.log(`Servidor está ouvindo na porta ${porta}`);
 });
